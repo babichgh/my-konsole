15d0cd1f877f752a0f0a3fd01cfbd1907b08ef3a
John Babich <166637847+babichgh@users.noreply.github.com>
2026-05-12 17:12:27 +0300

    Update install script for Starship and Fastfetch
    Moved font files and updated configuration for Starship and Fastfetch.

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo mv ./starship/BigBlueTerminal /usr/share/fonts/
+
 curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
+mv ./starship/starship.toml ~/.config/starship.toml
+
+sudo dnf install fastfetch -y
+fastfetch ---gen-config
+mv ./fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+mv ./fastfetch/logo.jpg ~/.config/fastfetch/
